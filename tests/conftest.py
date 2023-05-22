4e6da6fca3915a543d5a7d7c0d3b6514d487d94d
marc <mpucheniubo@gmail.com>
2023-05-22 13:52:18 +0200

    🧩 enlarged data set and added seed

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,10 +8,16 @@ from models.base import Base
 
 @pytest.fixture(scope="session")
 def base() -> Base:
+    np.random.seed(42)
     size = 100
     a = np.random.randint(0, 100, size=size)
     b = 100 * np.random.rand(size)
-    data = pd.DataFrame(data={"a":a, "b":b}, index=np.arange(size))
+    c = np.ones(size)
+    d = np.random.choice(["X", "Y", "Z"], size=size)
+    e = np.random.choice(["p", "q", "r", "s", "t"], size=size)
+    f = np.random.choice(["Y", "N"], size=size)
+    g = np.random.choice(["0", "1"], size=size)
+    data = pd.DataFrame(data={"a":a, "b":b, "c":c, "d":d, "e":e, "f":f, "g":g}, index=np.arange(size))
     data.reset_index(inplace=True)
     data.rename(columns={"index":"id"}, inplace=True)
     return Base(data=data, primary_key=["id"])
